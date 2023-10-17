fc0aaab23a3938c3bb60c782c58a30a60ecc1409
Acer <john.kosinski@gmail.com>
2023-10-17 18:28:43 +0700

    Latest contract addresses on 3 testnets

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,13 +1,43 @@
 
-export const addresses = {
+export const addresses: any = {
     zeroAddress: "0x0000000000000000000000000000000000000000",
-    securityManager: "0xe07675f0eF8de2B4f32b14FBfB602Ab2C04f091E",
-    nftFactory: "0x116de1c3808D19c37e8C56E5c0D220448702193b",
-    nftIssuer: '0x647Bf1c90C54BE4165BD3429718c01E0D7E61fB1',
-    nftStore: '0x99463902884A2b317868B264B08fa0D089678eb8',
-    zeppelin: '0x90e35EFE39613AAD98e97426F12c5F5E9720f8DA',
-    affiliatePayout: '0x65fA7EeC7CDEfA1e1CB45aC417D198929AE9a877', 
-    refundPolicy: '0x3E28659b3fAEed382A857F7D305701583fd290C6', 
-    financingPolicy: '0x251e3b3569820B9b4E1E93E57222a4d2Aa0F42ab', 
-    affiliatePolicy: '0x49347cBcD5451fEdd25eDC8a922198a75A523e3E'
+    sepolia: {
+        zeroAddress: "0x0000000000000000000000000000000000000000",
+        securityManager: "0xe07675f0eF8de2B4f32b14FBfB602Ab2C04f091E",
+        nftFactory: "0x116de1c3808D19c37e8C56E5c0D220448702193b",
+        nftStore: '0x99463902884A2b317868B264B08fa0D089678eb8',
+        nftIssuer: '0x647Bf1c90C54BE4165BD3429718c01E0D7E61fB1',
+        zeppelin: '0x90e35EFE39613AAD98e97426F12c5F5E9720f8DA',
+        affiliatePayout: '0x65fA7EeC7CDEfA1e1CB45aC417D198929AE9a877',
+        refundPolicy: '0x3E28659b3fAEed382A857F7D305701583fd290C6',
+        financingPolicy: '0x251e3b3569820B9b4E1E93E57222a4d2Aa0F42ab',
+        affiliatePolicy: '0x49347cBcD5451fEdd25eDC8a922198a75A523e3E',
+        policyFactory: '0xC6340610294aAC2c28771E6bc939ABe03CEd9426'
+    },
+    scroll: {
+        zeroAddress: "0x0000000000000000000000000000000000000000",
+        securityManager: "0xe21656269BB86877c6F90B287b7bb26EF9127640",
+        nftFactory: "0xA417775D7bCC5eA03D8F1223D25C3B47CB22C2bE",
+        nftStore: '0xd7e19b955321dcaE4070cF9240c2aD33C7439457',
+        nftIssuer: '0xADF789E61Bf38c463e4bA5B2B6E9C1Af6659e11b',
+        zeppelin: '0x412d0493c7fF6FFba527f2A8bcc21Fa419ea809C',
+        affiliatePayout: '0xE145471C075b5E57e85BcD909E9004fDD59557f5',
+        refundPolicy: '0xeAf00Aa7810bd658648E44eB059eaF15620E14e3',
+        financingPolicy: '0x3e6cecF46Ec03c570588454A634fdED70eD9b862',
+        affiliatePolicy: '0xCA5B040866eAf7aae577A05C664d702Df9a3C76d',
+        policyFactory: '0x7B362B300fb343a2DCc565580C65724bF156ac7F'
+    },
+    zkevm: {
+        zeroAddress: "0x0000000000000000000000000000000000000000",
+        securityManager: "0xe21656269BB86877c6F90B287b7bb26EF9127640",
+        nftFactory: "0xA417775D7bCC5eA03D8F1223D25C3B47CB22C2bE",
+        nftStore: '0xd7e19b955321dcaE4070cF9240c2aD33C7439457',
+        nftIssuer: '0x30EE26f15D25ceF2dfAb4e6381b4077bcE080771',
+        zeppelin: '0xf284B55F46B9A0Fe7d79ce92F869Ac52b9B3Ff7c',
+        affiliatePayout: '0x729895AaC3b1a776DcAd98c4d666e665d71a6E9F',
+        refundPolicy: '0x2209E03169D69F45bbADcbE230BBb3842F6c6c6E',
+        financingPolicy: '0xd86FFB404147163e19E010a0e9d4995E0e36F335',
+        affiliatePolicy: '0x2353640E45274cFdcbE550Bcb7bb2B9c382f7ab1',
+        policyFactory: '0xD48425B7fb702F571D872f4b7046B30c9FA47e15'
+    }
 }   
