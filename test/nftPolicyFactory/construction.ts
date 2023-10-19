72890b60f69135a1701f441c437885e0ff3a2fbd
Acer <john.kosinski@gmail.com>
2023-10-19 17:34:01 +0700

    Added test for Tableland related values

--- test/nftPolicyFactory/construction.ts
+++ test/nftPolicyFactory/construction.ts
@@ -19,8 +19,20 @@ describe("NftPolicyFactory: Construction", function () {
     });
 
     describe("Construction", function () {
-        it("initial property values", async function () {
+        it("initial property values default", async function () {
             expect(await policyFactory.securityManager()).to.equal(securityManager.target);
+            expect(parseInt(await policyFactory.policiesTableId())).to.equal(0);
+            expect(await policyFactory.supportsTableland()).to.equal(false);
+            expect(await policyFactory.policiesTablePrefix()).to.equal("");
+        });
+
+        it("initial property values with tableland", async function () {
+            const policyFactory2 = await deployNftPolicyFactory(securityManager.target, true, "policies", 11);
+            
+            expect(await policyFactory2.securityManager()).to.equal(securityManager.target);
+            expect(parseInt(await policyFactory2.policiesTableId())).to.equal(11);
+            expect(await policyFactory2.supportsTableland()).to.equal(true);
+            expect(await policyFactory2.policiesTablePrefix()).to.equal("policies");
         });
     });
 });
